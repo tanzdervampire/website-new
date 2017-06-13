adf5d0c593c44d34912f3e94d88c5a6b78344024
Ingo Bürk <ingo.buerk@tngtech.com>
2017-06-13 14:43:25 +0200

    Added react-toolbox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
+import theme from './toolbox/theme';
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
 
-class App extends Component {
-    render() {
-        return (
-            <div>
+import './toolbox/theme.css';
+
+const App = () => (
+    <ThemeProvider theme={theme}>
+        <div>
             <h2>Hello, World!</h2>
-            </div>
-        );
-    }
-}
+        </div>
+    </ThemeProvider>
+);
 
 export default App;
