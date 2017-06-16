141030b7a562d48b054b11511b56495547d10ae7
Ingo BÃ¼rk <ingo.buerk@tngtech.com>
2017-06-16 14:46:04 +0200

    Rewrite using only aggregation.

--- backend/routers/persons.js
+++ backend/routers/persons.js
@@ -1,42 +1,77 @@
 // @flow
 
 const router = require('express').Router();
-const Person = require('../models/person');
 const Show = require('../models/show');
 
-const getPersonToRolesMapping = () => {
+const findPersons = async () => {
     return Show.aggregate([
         /* We need one entry per cast member. */
         { $unwind: '$cast' },
+
         /* Reduce it to the only data we are interested in. */
         { $project: { role: '$cast.role', person: '$cast.person' } },
-        /* Group by person name into a set of roles. */
+
+        /* Group by person into a set of roles. */
         {
             $group: {
                 _id: '$person',
                 roles: { $addToSet: '$role' }
             }
-        }
+        },
+
+        /* Load the actual person information. */
+        {
+            $lookup: {
+                from: 'persons',
+                localField: '_id',
+                foreignField: '_id',
+                as: 'person'
+            }
+        },
+
+        /* $lookup returns person as an array, but it only has one value, so unwind it. */
+        { $unwind: '$person' },
+
+        /* Add the roles to the person sub-document. */
+        { $addFields: { 'person.roles': '$roles' } },
+
+        /* Replace the root since we now have everything in our person sub-document. */
+        { $replaceRoot: { newRoot: '$person' } },
+
+        /* Lastly, let's sort the list. */
+        { $sort: { name: 1 } }
     ]);
 };
 
-const findPersons = async (req, res) => {
-    const fields = req.query.fields ? req.query.fields.split(/,/) : [];
+const findRoles = () => {
+    return Show.aggregate([
+        /* We need one entry per cast member. */
+        { $unwind: '$cast' },
 
-    const documents = await Person.find({}).lean()
-        .sort({ name: 'ascending' });
+        /* Reduce it to the only data we are interested in. */
+        { $project: { role: '$cast.role', person: '$cast.person' } },
 
-    if (fields.includes('roles')) {
-        const mappings = await getPersonToRolesMapping();
-        documents.forEach(doc => {
-            const mapping = mappings.filter(m => m._id.toString() === doc._id.toString());
-            if (mapping && mapping.length === 1) {
-                doc.roles = mapping[0].roles;
+        /* Load the actual person information. */
+        {
+            $lookup: {
+                from: 'persons',
+                localField: 'person',
+                foreignField: '_id',
+                as: 'person'
             }
-        });
-    }
+        },
 
-    return documents;
+        /* $lookup returns person as an array, but it only has one value, so unwind it. */
+        { $unwind: '$person' },
+
+        /* Group by role into a set of persons. */
+        {
+            $group: {
+                _id: '$role',
+                persons: { $addToSet: '$person' }
+            }
+        }
+    ]);
 };
 
 /**
@@ -45,22 +80,33 @@ const findPersons = async (req, res) => {
  * GET
  * Returns a list of all persons.
  * This list is sorted alphabetically by name.
- *
- * Query parameters:
- *  fields
- *      Comma-separated list of additional fields to be returned.
- *      Possible values are 'roles'.
  */
 router.route('/')
     .get(async (req, res) => {
         try {
-            const documents = await findPersons(req, res);
+            const documents = await findPersons();
             return res.json(documents);
         } catch (err) {
             return res.send(err);
         }
     });
 
-// TODO FIXME Return list of people by role.
+// TODO FIXME Restrict by role
+// TODO FIXME Allow restricting to a production / location / …
+/**
+ * /roles
+ *
+ * GET
+ * Returns a list of roles and which persons have played this role.
+ */
+router.route('/roles')
+    .get(async (req, res) => {
+        try {
+            const documents = await findRoles();
+            return res.json(documents);
+        } catch (err) {
+            return res.send(err);
+        }
+    });
 
 module.exports = router;
