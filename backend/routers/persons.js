3b7427a14f221b450b3399ad9bac1c67bae492e1
Ingo Bürk <ingo.buerk@tngtech.com>
2017-06-16 14:04:39 +0200

    Additionally return played roles in /persons.

--- backend/routers/persons.js
+++ backend/routers/persons.js
@@ -2,6 +2,42 @@
 
 const router = require('express').Router();
 const Person = require('../models/person');
+const Show = require('../models/show');
+
+const getPersonToRolesMapping = () => {
+    return Show.aggregate([
+        /* We need one entry per cast member. */
+        { $unwind: '$cast' },
+        /* Reduce it to the only data we are interested in. */
+        { $project: { role: '$cast.role', person: '$cast.person' } },
+        /* Group by person name into a set of roles. */
+        {
+            $group: {
+                _id: '$person',
+                roles: { $addToSet: '$role' }
+            }
+        }
+    ]);
+};
+
+const findPersons = async (req, res) => {
+    const fields = req.query.fields ? req.query.fields.split(/,/) : [];
+
+    const documents = await Person.find({}).lean()
+        .sort({ name: 'ascending' });
+
+    if (fields.includes('roles')) {
+        const mappings = await getPersonToRolesMapping();
+        documents.forEach(doc => {
+            const mapping = mappings.filter(m => m._id.toString() === doc._id.toString());
+            if (mapping && mapping.length === 1) {
+                doc.roles = mapping[0].roles;
+            }
+        });
+    }
+
+    return documents;
+};
 
 /**
  * /
@@ -9,18 +45,22 @@ const Person = require('../models/person');
  * GET
  * Returns a list of all persons.
  * This list is sorted alphabetically by name.
+ *
+ * Query parameters:
+ *  fields
+ *      Comma-separated list of additional fields to be returned.
+ *      Possible values are 'roles'.
  */
 router.route('/')
     .get(async (req, res) => {
         try {
-            const documents = await Person.find({}).sort({ name: 'ascending' });
+            const documents = await findPersons(req, res);
             return res.json(documents);
         } catch (err) {
             return res.send(err);
         }
     });
 
-// TODO FIXME List of people participating in a specific production.
 // TODO FIXME Return list of people by role.
 
 module.exports = router;
