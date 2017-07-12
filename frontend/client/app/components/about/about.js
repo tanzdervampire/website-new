7808aea009e281f1e68229320f4c9a9e12696fc5
Ingo Bürk <ingo.buerk@tngtech.com>
2017-07-12 23:26:29 +0200

    Use new angular frontend (initial commit)

---
+++ frontend/client/app/components/about/about.js
@@ -0,0 +1,22 @@
+import angular from 'angular';
+import uiRouter from '@uirouter/angularjs';
+import aboutComponent from './about.component';
+
+let aboutModule = angular.module('about', [
+    uiRouter
+])
+
+    .config(($stateProvider) => {
+        'ngInject';
+        $stateProvider
+            .state('about', {
+                url: '/about',
+                component: 'about'
+            });
+    })
+
+    .component('about', aboutComponent)
+
+    .name;
+
+export default aboutModule;
