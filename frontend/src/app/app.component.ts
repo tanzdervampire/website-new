b3f7764a76abda2296391f657d16b3ee36ea3434
Ingo Bürk <ingo.buerk@tngtech.com>
2017-08-22 19:20:30 +0200

    Highlight active page.
    fixes #5

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,6 +6,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { ShowListPage } from '../pages/show-list/show-list';
 import { ActorListPage } from '../pages/actor-list/actor-list';
 
+interface PageInterface {
+    title: string;
+    name: string;
+    icon?: string;
+    component: Component;
+}
+
 @Component({
     templateUrl: 'app.html'
 })
@@ -14,16 +21,18 @@ export class MyApp {
 
     rootPage: any = ShowListPage;
 
-    pages: Array<{ title: string, icon?: string, component: any }>;
+    pages: PageInterface[];
+    activePage: PageInterface;
 
     constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
         this.initializeApp();
 
         this.pages = [
-            { title: 'Vorstellungen', icon: 'film', component: ShowListPage },
-            { title: 'Darsteller', icon: 'people', component: ActorListPage },
+            { title: 'Vorstellungen', icon: 'film', component: ShowListPage, name: 'ShowListPage' },
+            { title: 'Darsteller', icon: 'people', component: ActorListPage, name: 'ActorListPage' },
         ];
 
+        this.activePage = this.pages[0];
     }
 
     initializeApp() {
@@ -35,7 +44,13 @@ export class MyApp {
         });
     }
 
-    openPage(page) {
+    openPage(page: PageInterface) {
+        this.activePage = page;
         this.nav.setRoot(page.component);
     }
+
+    isActive(page: PageInterface): string {
+        return this.activePage === page ? 'primary' : null;
+    }
+
 }
