f4413aca576253211006119082c3c08a30730c09
Ingo Bürk <ingo.buerk@tngtech.com>
2017-08-26 19:23:41 +0200

    Add show submission to sub-navigation

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,21 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Nav, NavController, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { ShowListPage } from '../pages/show-list/show-list';
 import { ActorListPage } from '../pages/actor-list/actor-list';
+import { ShowSubmitStartPage } from '../pages/show-submit-start/show-submit-start';
 
 interface PageInterface {
     title: string;
-    name: string;
     icon?: string;
     component: Component;
+    name: string;
+}
+
+interface MainPageInterface extends PageInterface {
+    subPages?: PageInterface[];
 }
 
 @Component({
@@ -21,21 +26,29 @@ export class MyApp {
 
     rootPage: any = ShowListPage;
 
-    pages: PageInterface[];
-    activePage: PageInterface;
+    pages: MainPageInterface[];
+    activePage: MainPageInterface;
 
-    constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+    constructor(
+        public platform: Platform,
+        public statusBar: StatusBar,
+        public splashScreen: SplashScreen) {
+
         this.initializeApp();
 
+        const showSubPages = [
+            { title: 'Vorstellung eintragen', icon: 'add', component: ShowSubmitStartPage, name: 'ShowSubmitStartPage' },
+        ];
+
         this.pages = [
-            { title: 'Vorstellungen', icon: 'film', component: ShowListPage, name: 'ShowListPage' },
+            { title: 'Vorstellungen', icon: 'film', component: ShowListPage, name: 'ShowListPage', subPages: showSubPages },
             { title: 'Darsteller', icon: 'people', component: ActorListPage, name: 'ActorListPage' },
         ];
 
         this.activePage = this.pages[0];
     }
 
-    initializeApp() {
+    initializeApp(): void {
         this.platform.ready().then(() => {
             // Okay, so the platform is ready and our plugins are available.
             // Here you can do any higher level native things you might need.
@@ -44,12 +57,16 @@ export class MyApp {
         });
     }
 
-    openPage(page: PageInterface) {
+    openPage(page: MainPageInterface): void {
         this.activePage = page;
         this.nav.setRoot(page.component);
     }
 
-    isActive(page: PageInterface): string {
+    openSubPage(page: PageInterface): void {
+        this.nav.push(page.component);
+    }
+
+    isActive(page: MainPageInterface): string {
         return this.activePage === page ? 'primary' : null;
     }
 
